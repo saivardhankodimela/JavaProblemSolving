9494383804babcf48de0a55ddc54bde24802fe75
Sai Vardhan Kodimela <66217959+saivardhankodimela@users.noreply.github.com>
2022-09-15 16:43:36 +0530

    Create Q1.java

---
+++ Day1/Q1.java
@@ -0,0 +1,13 @@
+//Since starting could be hard sometimes, let us try the simplest problem possible. The goal is to have practice on submitting answers etc.
+//We need to sum two numbers and tell the result. Though you can do it manually, try to write a simple program in any language you know, or like, or want to learn.
+
+import java.util.Scanner;
+public class Q1{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    System.out.println(num1+num2);
+  }
+}  
